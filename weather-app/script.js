95dcd47d99f009127019088e8547f2a4c1756b42
Pranshu Sama <23je0733@iitism.ac.in>
2026-06-19 20:44:01 +0530

    script file for weather app

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,9 +1,17 @@
 const button = document.getElementById('search-button');
 const inputCity = document.getElementById('city-input');
 
+const cityName = document.getElementById('city-name');
+const cityTime = document.getElementById('city-time');
+const cityTemp = document.getElementById('city-temp');
+
+
+
+
+
 async function getData(cityName){
     const promise = await fetch(
-        ``
+        `http://api.weatherapi.com/v1/current.json?key=589008170c714e7dbb2105645261906&q=${cityName}&aqi=yes`
     );
     return await promise.json();
 }
@@ -11,5 +19,8 @@ async function getData(cityName){
 button.addEventListener("click", async() => {
     const value = inputCity.value;
     const result = await getData(value);
-    console.log(result);
+
+    cityName.innerText = `${result.location.name} , ${result.location.region} - ${result.location.country}`;
+    cityTime.innerText = result.location.localtime;
+    cityTemp.innerText = result.current.temp_c; 
 });
